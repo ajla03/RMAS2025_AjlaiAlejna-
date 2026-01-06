1ce22833aebf022b1c74c732bb41c71a21ee5889
alejna-hodzic <alejna.hodzic@fet.ba>
2026-01-06 20:41:38 +0100

    separation

--- projekat/projekatFaza23/app/src/main/java/com/example/projekatfaza23/model/LoginUIState.kt
+++ projekat/projekatFaza23/app/src/main/java/com/example/projekatfaza23/model/LoginUIState.kt
@@ -13,36 +13,3 @@ data class LoginUIState(
     val errorMessage: String?= null
 
 )
-private val mockUsers = mapOf(
-    "admin@test.com" to "admin123",
-    "korisnik@hr.ba" to "lozinka123"
-)
-class  LoginViewModel : ViewModel(){
-    private val _uiState = MutableStateFlow(LoginUIState())
-    val uiState = _uiState.asStateFlow()
-
-    fun updateEmail(email : String){
-        _uiState.update { it.copy(email = email) }
-    }
-
-    fun updatePassword(password: String){
-        _uiState.update { it.copy(password = password) }
-    }
-
-    fun login(){
-        val currentEmail = _uiState.value.email
-        val currentPassword  = _uiState.value.password
-
-        _uiState.update { it.copy(isLoading = true) }
-
-        if(mockUsers[currentEmail]==currentPassword){
-            _uiState.update { it.copy(isLoading = false, isLoginSuccessful = true) }
-        }else{
-            _uiState.update {
-                it.copy(isLoginSuccessful = false,
-                        isLoading = false,
-                        errorMessage = "Pogresan email ili lozinka!")
-            }
-        }
-    }
-}
---
+++ projekat/projekatFaza23/app/src/main/java/com/example/projekatfaza23/model/LoginViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.projekatfaza23.model
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+private val mockUsers = mapOf(
+    "admin@test.com" to "admin123",
+    "korisnik@hr.ba" to "lozinka123"
+)
+class  LoginViewModel : ViewModel(){
+    private val _uiState = MutableStateFlow(LoginUIState())
+    val uiState = _uiState.asStateFlow()
+
+    fun updateEmail(email : String){
+        _uiState.update { it.copy(email = email) }
+    }
+
+    fun updatePassword(password: String){
+        _uiState.update { it.copy(password = password) }
+    }
+
+    fun login(){
+        val currentEmail = _uiState.value.email
+        val currentPassword  = _uiState.value.password
+
+        _uiState.update { it.copy(isLoading = true) }
+
+        if(mockUsers[currentEmail]==currentPassword){
+            _uiState.update { it.copy(isLoading = false, isLoginSuccessful = true) }
+        }else{
+            _uiState.update {
+                it.copy(isLoginSuccessful = false,
+                    isLoading = false,
+                    errorMessage = "Pogresan email ili lozinka!")
+            }
+        }
+    }
+}
